643df8bc897204746a32704e1b457bf0ea03cede
AmritParashar1 <145751993+AmritParashar1@users.noreply.github.com>
2024-08-11 23:38:29 +0530

    Time: 0 ms (100%), Space: 14 MB (19.4%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
@@ -1,87 +1,44 @@
 class Solution {
 public:
-
-    int pivotindex(vector<int>& nums)
-    {
-        int n = nums.size();
-        int s=0;
-        int e=n-1;
-        int mid = s+(e-s)/2;
-
-        while(s<=e)
-        {
-            if(s==e)
-            {
-                return s;
-            }
-
-            if( mid-1>=0 && nums[mid]<nums[mid-1])
-            {
-                return mid-1;
-            }
-
-            else if(mid+1<n && nums[mid]>nums[mid+1])
-            {
-                return mid;
-            }
-
-            else if(nums[s]>nums[mid])
-            {
-                e = mid-1;
-            }
-            
-            else
-            {
-                s = mid+1;
-            }
-
-            mid = s + (e-s)/2;
-        }
-
-        return -1;
-    }
-
-    int binarysearch(vector<int>nums,int s , int e, int target)
-    {
-        int mid = s + (e-s)/2;
-        while(s<=e)
-        {
-            if(nums[mid]==target)
-            {
-                return mid;
-            }
-
-            else if (nums[mid]>target)
-            {
-                e = mid -1 ;
-            }
-
-            else
-            {
-                s = mid+1;
-            }
-
-            mid = s + (e-s)/2;
-        }
-        return -1;
-    }
     int search(vector<int>& nums, int target) {
 
-      int pivot =  pivotindex(nums);
-      int n = nums.size();
-      int ans = -1;
+        int n = nums.size();
 
-      if(target >= nums[0] && target <= nums[pivot])
-      {
-        ans = binarysearch(nums,0,pivot,target);
-      }
+        int low = 0; int high = n-1;
+        while(low<=high)
+        {
+            int mid = low + (high - low)/2;
 
-      else
-      {
-        ans = binarysearch(nums,pivot+1,n-1,target);
-      }
+            if(nums[mid]==target) return mid;
 
-      return ans;
+            if(nums[low]<=nums[mid])
+            {
+                if(nums[low]<=target && target <= nums[mid])
+                {
+                    high = mid - 1;
+                }
 
+                else 
+                {
+                    low = mid+1;
+                }
+            }
+
+            else
+            {
+                if(nums[mid] <= target && target <= nums[high])
+                {
+                    low = mid + 1;
+                }
+
+                else
+                {
+                    high = mid - 1;
+                }
+
+            }
+        }
+        return -1;
+        
     }
 };
