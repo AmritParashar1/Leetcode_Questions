761a8971ab9fcec2f596dc24bad99f53583c73ef
AmritParashar1 <145751993+AmritParashar1@users.noreply.github.com>
2024-08-15 20:21:09 +0530

    Time: 3 ms (65.01%), Space: 11.6 MB (7.48%) - LeetHub

--- 0162-find-peak-element/0162-find-peak-element.cpp
+++ 0162-find-peak-element/0162-find-peak-element.cpp
@@ -5,24 +5,30 @@ public:
 
         if(n==1) return 0;
 
-        for(int i=0;i<n;i++)
+        if(arr[0] > arr[1]) return 0;
+        if(arr[n-1]>arr[n-2]) return n-1;
+
+        int low = 1; int high = n-2;
+
+        while(low<=high)
         {
-            if(i==0)
+            int mid = low + (high - low)/2;
+
+            if(arr[mid]> arr[mid-1] && arr[mid] > arr[mid+1])
             {
-                if(arr[i] > arr[i+1]) return i;
+                return mid;
             }
 
-            else if (i==n-1)
-            {
-                if(arr[i] > arr[i-1]) return i;
-            }
+            //visualise kar le bhai yaha pe , pichli baar visualise karke
+            // click kar gaya tha
 
+            else if(arr[mid] > arr[mid-1])
+            {
+                low = mid+1;
+            }
             else
             {
-                if(arr[i] > arr[i+1] && arr[i] > arr[i-1])
-                {
-                    return i;
-                }
+                high = mid-1;
             }
         }
         return -1;
