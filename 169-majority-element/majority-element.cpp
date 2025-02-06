a0a7acb74afdc54a3f1f3cbdc7cb6f4c20447d14
Amrit Parashar <145751993+AmritParashar1@users.noreply.github.com>
2025-02-06 23:32:07 +0530

    Time: 0 ms (100.00%) | Memory: 28.2 MB (24.49%) - LeetSync

---
+++ 169-majority-element/majority-element.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int majorityElement(vector<int>& nums) {
+        int n = nums.size();
+
+        int major = nums[0]; int count = 1;
+
+        for(int i=1;i<n;i++) {
+            if(count==0) {
+                major = nums[i];
+                count = 1;
+            }
+            else if(nums[i] == major) {
+                count++;
+            }
+            else {
+                count--;
+            }
+        }
+
+        return major;
+
+
+        
+    }
+};
